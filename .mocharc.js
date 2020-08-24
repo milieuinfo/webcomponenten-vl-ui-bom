2ad160f45059b06e1442f559e14c566c29edf01f
Tom Coemans <tom.coemans@gmail.com>
2020-08-24 17:59:17 +0200

    exclude action-group and map-search tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion|action-group|map-search).test.js"],
     "timeout": '10000'
 };
