f4c50dc0cda667dd484aeda3cdc35ca223ce7283
Tom Coemans <tom.coemans@gmail.com>
2020-08-24 18:09:05 +0200

    exclude vl-alert

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion|action-group|map-search).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion|action-group|map-search|alert).test.js"],
     "timeout": '10000'
 };
