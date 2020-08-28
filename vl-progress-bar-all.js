dc6ee7139e125bf92ea6c4770d16a1ed4c9bd881
Tom Coemans <tom.coemans@gmail.com>
2020-08-28 12:48:33 +0200

    removed generated code

--- vl-progress-bar-all.js
+++
@@ -1,7 +0,0 @@
-import {VlProgressBar} from '/node_modules/vl-ui-progress-bar/dist/vl-progress-bar.js';
-import {VlProgressBarStep} from '/node_modules/vl-ui-progress-bar/dist/vl-progress-bar-step.js';
-
-export {
-  VlProgressBar,
-  VlProgressBarStep,
-};
--- vl-search-results-all.js
+++
@@ -1,7 +0,0 @@
-import {VlSearchResult} from '/node_modules/vl-ui-search-results/dist/vl-search-result.js';
-import {VlSearchResults} from '/node_modules/vl-ui-search-results/dist/vl-search-results.js';
-
-export {
-  VlSearchResult,
-  VlSearchResults,
-};
