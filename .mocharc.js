5177e112bc0723220c28610cb06b40680937bfd6
Tom Coemans <tom.coemans@gmail.com>
2020-09-09 12:38:49 +0200

    exclude modal tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion|action-group|map-search|alert).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/!(wizard|accordion|action-group|map-search|alert|modal).test.js"],
     "timeout": '10000'
 };
