7d4bf5285d067f0abd74ab7b1c9fd377acb21751
cambiph <philippe.cambien@gmail.com>
2020-10-23 09:51:39 +0200

    Schakel over op multireporter

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,9 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'spec',
+    "reporter": 'mocha-multi-reporters',
+    "reporter-option": [
+        "configFile=reporter-config.json"
+    ],
     "spec": ["node_modules/vl-ui-*/test/e2e/*.test.js"],
     "timeout": '20000'
 };
