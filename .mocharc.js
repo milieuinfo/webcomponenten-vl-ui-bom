27765b9e1f7ab2925bd4871b314ce444d92bd212
Tom Coemans <tom.coemans@gmail.com>
2020-12-14 12:10:47 +0100

    exclude form-valiation e2e tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/e2e/*.test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/!(form-validation).test.js"],
     "timeout": '200000'
 };
