43e264520e7dd8729571e75ca750195bfca7a4a2
Tom Coemans <tom.coemans@gmail.com>
2020-12-14 11:08:14 +0100

    changed mocharc spec path

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["test/**/*.test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/*.test.js"],
     "timeout": '10000'
 };
