448265778820e4d4213d2564d45aab3c104687c3
Tom Coemans <tom.coemans@gmail.com>
2020-12-14 11:03:59 +0100

    changed mocharc config

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,6 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'mocha-multi-reporters',
-    "reporter-option": [
-        "configFile=reporter-config.json"
-    ],
-    "spec": ["node_modules/vl-ui-*/test/e2e/*.test.js"],
-    "timeout": '20000'
+    "reporter": 'spec',
+    "spec": ["test/**/*.test.js"],
+    "timeout": '10000'
 };
