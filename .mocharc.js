eb56633625afd0a4a35674932ca9464688cacfc3
coemans <tom.coemans@vlaanderen.be>
2020-12-17 09:10:21 +0100

    Upgrade huisstijl versie 3.12.3 (#15)

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,9 @@
 module.exports = {
     "full-trace": true,
-    "reporter": 'mocha-multi-reporters',
-    "reporter-option": [
-        "configFile=reporter-config.json"
-    ],
+    "reporter": "mocha-multi-reporters",
+    "reporterOptions": {
+        "configFile": 'reporter-config.json'
+    },
     "spec": ["node_modules/vl-ui-*/test/e2e/*.test.js"],
-    "timeout": '20000'
+    "timeout": '50000'
 };
